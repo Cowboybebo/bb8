56395755b31880f9fa172b5abbfe5e40bbb546f3
Cowboybebo <32975470+Cowboybebo@users.noreply.github.com>
2017-10-21 00:05:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Prefix = "/"
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Logged in as ${client.user.tag}!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+client.on('message', msg => {
+    if (msg.content === Prefix + 'Creator') {
+    	msg.reply('My creator is LimitedMine(Adam) C:');
   	}
 });
+client.on('message', msg => {
+  if (msg.content === Prefix + 'info') {
+    msg.reply('Hello im PreNetwork Bot! I was Made By PreNetwork xD C:');
+  }
+});
 
+client.on('message', msg => {
+  if (msg.content === Prefix + 'Commands') {
+    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === Prefix + 'commands') {
+    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === Prefix + 'Test') {
+    msg.reply('Marcus is a creep!');
+  }
+});
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login('MzcwNzcwMzAwMDA0MjA0NTQ3.DMr6Rw.P8Ahxicps6lHbCHwtTG_22TT6uY');
